79bfa205b3da50821da78cdcb3399f02fc2cc95c
popcornn95 <danishdavis@hotmail.com>
2020-01-16 21:39:33 -0500

    added styling to the homepage

--- src/components/menu-items/Menu-items.components.jsx
+++ src/components/menu-items/Menu-items.components.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './Menu-items.styles.scss'
 
 const MenuItem = ({ section }) => (
-    <div style={{ backgroundImage: `url(${section.imageUrl})` }} className={`${section.size} menu-item`} >
+    <div className={`${section.size} menu-item`} >
+        <div className="background-image" style={{ backgroundImage: `url(${section.imageUrl})` }}></div>
         <div className='content'>
             <h1 className='title'>{section.title}</h1>
             <span className='subtitle'>SHOP NOW</span>
