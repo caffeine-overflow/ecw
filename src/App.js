850b7bfd286d4f571de7516a021fcfb2ee6a8175
popcornn95 <danishdavis@hotmail.com>
2020-01-19 19:31:10 -0500

    Bug fixes and added nvabar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Homepage from './Pages/homepage/Homepage.component'
 import ShopPage from './Pages/shop/shop.component'
+import Header from './components/header/header.component'
 import './App.css';
 
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
